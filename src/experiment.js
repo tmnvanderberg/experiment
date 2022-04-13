cea878f1d23c0ae5033d50c8fdc6ac6cc5ef21c3
Priscila B. Borges <34509696+prisciencebug@users.noreply.github.com>
2022-04-13 18:21:31 +0200

    add subject, instruction vars

--- src/experiment.js
+++ src/experiment.js
@@ -1,5 +1,5 @@
 /**
- * @title Semantic Memory Experiment
+ * @title Semantic Memory Task
  * @description <todo>
  * @version 0.1.0
  *
@@ -22,6 +22,10 @@ import { initJsPsych } from "jspsych";
 import SemanticMemoryTaskPlugin from "./plugin";
 import PreloadPlugin from "@jspsych/plugin-preload";
 import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+import jsPsychSurveyText from '@jspsych/plugin-survey-text';
+import jsPsychInstructions from '@jspsych/plugin-instructions';
+
+
 
 /**
  * This method will be executed by jsPsych Builder and is expected to run the
@@ -53,6 +57,29 @@ export async function run({ assetPaths, input = {}, environment }) {
     choices: "a",
   };
 
+  
+  var subject = {
+    type: jsPsychSurveyText,
+    questions: [
+      {
+        prompt:
+          "Bitte schalten Sie Musik, Handys und andere Geräte, die Sie ablenken könnten, aus. <p>Geben Sie unten Ihre Versuchspersonennummer ein: </p>",
+        required: true,
+      },
+    ],
+    button_label: "Weiter"
+    };
+
+  var instructions = {
+    type: jsPsychInstructions,
+    pages: [
+    '<p><strong> Anweisungen</strong></p><p>Bitte wählen Sie das Bild aus der unteren Vierergruppe aus, das am besten zu dem oberen Bild passt. <p> Danach geben Sie bitte ein Wort ein, das am besten beschreibt, wie das von Ihnen gewählte Bild mit dem oberen Bild zusammenhängt.</p><p>Klicken Sie auf "Weiter", wenn Sie bereit sind, zu beginnen.</p>'
+    ],
+    button_label_next: "Weiter",
+    button_label_previous: "Zurück",
+    show_clickable_nav: true
+}
+
   const items = require("../items/first.json");
   const semanticMemoryExperiment = {
     timeline: [
@@ -75,7 +102,7 @@ export async function run({ assetPaths, input = {}, environment }) {
     error_message: "The experiment failed to load. Please contact the researcher.",
   };
 
-  await jsPsych.run([preload, welcomeText, semanticMemoryExperiment]);
+  await jsPsych.run([preload, subject, welcomeText, instructions, semanticMemoryExperiment]);
 
   return jsPsych;
 }
